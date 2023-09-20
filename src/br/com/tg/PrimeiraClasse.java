61c5af28a2663c0f5255a16ce1fcaafa558c922e
Tomas Gonzalez <tomnas234@gmail.com>
2023-09-20 11:27:54 -0400

    Entregando a tarefa do curso de especialista java

---
+++ src/br/com/tg/PrimeiraClasse.java
@@ -0,0 +1,14 @@
+package br.com.tg;
+
+public class PrimeiraClasse {
+
+	public static void main(String[] args) {
+		String ambiente = args [0];
+		
+		if(ambiente.equals("DEV")) {
+			System.out.println("DEV");
+		}
+
+	}
+
+}
